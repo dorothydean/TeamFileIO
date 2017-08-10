54df7a2e5ec1bf11bfd40aae0af62bebae44803b
Dorothy Dean <durabledora@gmail.com>
2017-08-10 15:56:11 -0700

    Created repo and added form and methods

--- TeamFileIO/Form1.Designer.cs
+++ TeamFileIO/Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtImport = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.cboxDecrypt = new System.Windows.Forms.CheckBox();
+            this.gboxImport = new System.Windows.Forms.GroupBox();
+            this.gboxSave = new System.Windows.Forms.GroupBox();
+            this.cboxEncrypt = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtSave = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.gboxImport.SuspendLayout();
+            this.gboxSave.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtImport
+            // 
+            this.txtImport.Location = new System.Drawing.Point(39, 65);
+            this.txtImport.Name = "txtImport";
+            this.txtImport.Size = new System.Drawing.Size(324, 31);
+            this.txtImport.TabIndex = 0;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(416, 65);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(149, 49);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // cboxDecrypt
+            // 
+            this.cboxDecrypt.AutoSize = true;
+            this.cboxDecrypt.Location = new System.Drawing.Point(598, 76);
+            this.cboxDecrypt.Name = "cboxDecrypt";
+            this.cboxDecrypt.Size = new System.Drawing.Size(213, 29);
+            this.cboxDecrypt.TabIndex = 2;
+            this.cboxDecrypt.Text = "Decrypt on import";
+            this.cboxDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // gboxImport
+            // 
+            this.gboxImport.Controls.Add(this.cboxDecrypt);
+            this.gboxImport.Controls.Add(this.btnImport);
+            this.gboxImport.Controls.Add(this.txtImport);
+            this.gboxImport.Location = new System.Drawing.Point(35, 28);
+            this.gboxImport.Name = "gboxImport";
+            this.gboxImport.Size = new System.Drawing.Size(806, 164);
+            this.gboxImport.TabIndex = 3;
+            this.gboxImport.TabStop = false;
+            this.gboxImport.Text = "Import File";
+            // 
+            // gboxSave
+            // 
+            this.gboxSave.Controls.Add(this.cboxEncrypt);
+            this.gboxSave.Controls.Add(this.txtSave);
+            this.gboxSave.Controls.Add(this.btnSave);
+            this.gboxSave.Location = new System.Drawing.Point(35, 260);
+            this.gboxSave.Name = "gboxSave";
+            this.gboxSave.Size = new System.Drawing.Size(806, 164);
+            this.gboxSave.TabIndex = 4;
+            this.gboxSave.TabStop = false;
+            this.gboxSave.Text = "Save File";
+            // 
+            // cboxEncrypt
+            // 
+            this.cboxEncrypt.AutoSize = true;
+            this.cboxEncrypt.Location = new System.Drawing.Point(598, 79);
+            this.cboxEncrypt.Name = "cboxEncrypt";
+            this.cboxEncrypt.Size = new System.Drawing.Size(193, 29);
+            this.cboxEncrypt.TabIndex = 2;
+            this.cboxEncrypt.Text = "Encrypt onsave";
+            this.cboxEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(416, 68);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(149, 49);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtSave
+            // 
+            this.txtSave.Location = new System.Drawing.Point(39, 68);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(324, 31);
+            this.txtSave.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(350, 450);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(207, 99);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 576);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.gboxImport);
+            this.Controls.Add(this.gboxSave);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gboxImport.ResumeLayout(false);
+            this.gboxImport.PerformLayout();
+            this.gboxSave.ResumeLayout(false);
+            this.gboxSave.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtImport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.CheckBox cboxDecrypt;
+        private System.Windows.Forms.GroupBox gboxImport;
+        private System.Windows.Forms.GroupBox gboxSave;
+        private System.Windows.Forms.CheckBox cboxEncrypt;
+        private System.Windows.Forms.TextBox txtSave;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- TeamFileIO/Form1.cs
+++ TeamFileIO/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,57 @@ namespace TeamFileIO
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            //choose file
+            ImportFile();
+            
+            //encrypt
+        }
+
+        private void ImportFile()
+        {
+            var importFile = new OpenFileDialog();
+            importFile.ShowDialog();
+            MessageBox.Show("File successfully imported!");
+            //todo add if else for success or failure of import
+        }
+
+        private void EncryptOrDecryptFile()
+        {
+            if (cboxDecrypt.Checked)
+            {
+                //decrypt file
+            }
+
+            if (cboxEncrypt.Checked)
+            {
+                //encrypt file
+            }
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var saveFile = new SaveFileDialog();
+            saveFile.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<String> file = new List<string>();
+            file.Add("string Hey there joe!");
+            string path = @"C:test";
+            File.AppendAllLines(path, file);
+            
+        }
+
+
     }
 }
